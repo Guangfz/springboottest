d4e3f4af15a9b918f4c7ae1916caf626529ecc1c
guangfz <987962174@qq.com>
2019-12-10 20:05:39 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/springboot/SpringBoot01HellowordQuikApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HellowordQuikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01HellowordQuikApplication.class, args);
+    }
+
+}
